e45675062fe43b8bd76226b6f2e13b459f56cd22
MichaelTempest <MichaelTempest@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-07-02 20:54:48 +0000

    Item1509: Set svn:keywords property to "Rev" so that the VERSION will be automatically updated in future
    git-svn-id: http://svn.foswiki.org/trunk/DirectedGraphWebMapPlugin@4391 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/DirectedGraphWebMapPlugin.pm
+++ lib/Foswiki/Plugins/DirectedGraphWebMapPlugin.pm
@@ -19,10 +19,10 @@ require Foswiki::Plugins;    # For the API version
 
 # $VERSION is referred to by Foswiki, and is the only global variable that
 # *must* exist in this package.
-# This should always be $Rev: 1340 $ so that Foswiki can determine the checked-in
+# This should always be $Rev$ so that Foswiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
 # you should leave it alone.
-our $VERSION = '$Rev: 1340 $';
+our $VERSION = '$Rev$';
 
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
